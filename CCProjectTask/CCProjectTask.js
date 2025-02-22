6ea7ca465f035b29c87023963648227cfc1729e6
ls <emailjerom@gmail.com>
2025-02-22 19:01:13 +0100

    last comit : final version

--- CCProjectTask/CCProjectTask.js
+++ CCProjectTask/CCProjectTask.js
@@ -7,5 +7,11 @@ const messagesArray = {
     astroMessage : ['Today will be a lucky day', 'You will meet Love today', 'Be careful with your boss', 'You have a secret Lover', 'Check your health', 'Money will come to you', 'Beware if you take your care today'],
 }
 
+function futureTelling () {
+    const sign = messagesArray.astroSign[randomChoice()];
+    const message = messagesArray.astroMessage[randomChoice()];
+    return `You are ruled by the ${sign}. ${message}`;
+}
 
+console.log(futureTelling());
 
