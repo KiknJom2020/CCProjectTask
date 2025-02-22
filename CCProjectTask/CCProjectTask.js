4d0cfa8e04d3f03761ceb2bfb4f8f00660bc7636
ls <emailjerom@gmail.com>
2025-02-22 18:30:45 +0100

    3rd commit : corrected syntax errors

--- CCProjectTask/CCProjectTask.js
+++ CCProjectTask/CCProjectTask.js
@@ -1,10 +1,11 @@
-let randomChoice = num => {
-    return Math.floor(Math.random() * num);
+function randomChoice() {
+    return Math.floor(Math.random() * 6);
 }
 
-const messagesArray {
+const messagesArray = {
     astroSign : ['leo', 'pisces', 'balance', 'taurus', 'gemini', 'virgo'],
     astroMessage : ['Today will be a lucky day', 'You will meet Love today', 'Be careful with your boss', 'You have a secret Lover', 'Check your health', 'Money will come to you', 'Beware if you take your care today'],
-
 }
+
+
 
